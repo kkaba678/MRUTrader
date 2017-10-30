b13ae732c427082994da69ffc074ef3c3d5a4d18
quantumparadigm <Q.paradigm@hotmail.com>
2017-10-30 00:05:30 -0600

    Initial commit

---
+++ app/src/main/java/mrutrader/mrutrader/Register_Activity.java
@@ -0,0 +1,138 @@
+package mrutrader.mrutrader;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v4.text.TextUtilsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+public class Register_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE = 1;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private Button buttonRegister;
+    private Button buttonLogin;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private ProgressDialog progressDialog;
+    private String userName;
+    private static final String TAG = "MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonRegister.setOnClickListener(this);
+
+        buttonLogin.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void registerUser(){
+        String userName = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        String email = userName + "@mtroyal.ca";
+
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register_Activity.this, "Registration Successful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                            Toast.makeText(Register_Activity.this, "Failed to register user",
+                                    Toast.LENGTH_SHORT).show();
+
+
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            registerUser();
+
+        }
+
+        if(v == buttonLogin){
+
+        }
+    }
+}
