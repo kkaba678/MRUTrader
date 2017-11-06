d5833dcebd9f2552cb4812bed630ff5a4ba8c366
quantumparadigm <Q.paradigm@hotmail.com>
2017-11-06 14:06:22 -0700

    Created layour of Profile activity and Add Course activity

--- app/src/main/java/mrutrader/mrutrader/AddCourseActivity.java
+++ app/src/main/java/mrutrader/mrutrader/AddCourseActivity.java
@@ -1,21 +1,131 @@
 package mrutrader.mrutrader;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class AddCourseActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.R.attr.dashGap;
+import static android.R.attr.name;
+
+public class AddCourseActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public class Course {
+
+        private String userID;
+        private String courseName;
+        private int price;
+        private boolean textBook;
+        private boolean lectureNotes;
+        private boolean tutoring;
+
+
+        public Course() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public Course(String userID, String courseName,int price, boolean textBook, boolean lectureNotes, boolean tutoring) {
+            this.userID = userID;
+            this.courseName = courseName;
+            this.price = price;
+            this.textBook = textBook;
+            this.lectureNotes = lectureNotes;
+            this.tutoring = tutoring;
+        }
+
+        @Override
+        public String toString() {
+            return this.userID + ": " + this.courseName;
+        }
+
+    }
+
+    private static final String TAG = "AddCourseActivity";
+    EditText courseNameEditText;
+    SearchView simpleSearchView;// = (SearchView) findViewById(R.id.simpleSearchView);
+    Button button2;
+    DatabaseReference mDatabase;
+    EditText priceEditText;
+    RadioButton textBookRadioButton;
+    RadioButton notesRadioButton;
+    RadioButton tutorRadioButton;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
-
         Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+
+        userID = b.getString("userID");
     }
 
-    public void finish(){
 
+    @Override
+    public void onClick(View v){
+        button2 = (Button) findViewById(R.id.button2);
+        courseNameEditText = (EditText) findViewById(R.id.courseNameEditText);
+        priceEditText = (EditText) findViewById(R.id.priceEditText);
+        textBookRadioButton = (RadioButton) findViewById(R.id.TextBookRadioButton);
+        notesRadioButton = (RadioButton) findViewById(R.id.notesRadioButton);
+        tutorRadioButton = (RadioButton) findViewById(R.id.tutorRadioButton);
+
+        String cName = courseNameEditText.getText().toString();
+        String ePrice = priceEditText.getText().toString();
+        try {
+
+            int price = Integer.parseInt(ePrice);
+            boolean textBook = textBookRadioButton.isChecked();
+            boolean notes = notesRadioButton.isChecked();
+            boolean tutoring = tutorRadioButton.isChecked();
+            Course course = new Course(userID, cName, price, textBook, notes, tutoring);
+
+            mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+            Map<String, Object> taskMap = new HashMap<>();
+            taskMap.put("userID", userID);
+            taskMap.put("course name", course.courseName);
+            taskMap.put("price", course.price);
+            taskMap.put("textBook", course.textBook);
+            taskMap.put("notes", course.lectureNotes);
+            taskMap.put("tutoring", course.tutoring);
+            mDatabase.child("Course").child(userID).push().setValue(taskMap);
+        }
+        catch(NumberFormatException e){
+            e.getMessage();
+        }
+
+        finish();
+
+    }
+
+    @Override
+    public void finish() {
+        // Prepare data intent
+        Intent data = getIntent();
+
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, data);
         super.finish();
     }
+
 }
--- app/src/main/java/mrutrader/mrutrader/ProfileActivity.java
+++ app/src/main/java/mrutrader/mrutrader/ProfileActivity.java
@@ -4,17 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
-
     private static final int REQUEST_CODE = 1;
-    private FirebaseAuth fireBaseAuth;
+    private FirebaseUser user;
+    private String userID;
+
+    private ArrayList<String> list;
+    private ArrayAdapter adapter;
+
 
 
     @Override
@@ -22,33 +41,113 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        fireBaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuth fireBaseAuth = FirebaseAuth.getInstance();
 
-        if(fireBaseAuth.getCurrentUser() == null){
+        user = fireBaseAuth.getCurrentUser();
 
+        if (user != null) {
+            userID = user.getUid();
+        }
+    }
+
+    private void collectCourses(Map<String,Object> courses) {
+
+        ArrayList<Long> courseName = new ArrayList<>();
+
+        //iterate through each user, ignoring their UID
+        for (Map.Entry<String, Object> entry : courses.entrySet()){
+
+            //Get user map
+            Map singleCourse = (Map) entry.getValue();
+            //Get phone field and append to list
+            courseName.add((Long) singleCourse.get("userID"));
+            adapter.add(courseName.get(0));
         }
 
-        FirebaseUser user = fireBaseAuth.getCurrentUser();
     }
 
-    public void addCourse(View v){
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference()
+                .child("Course").child(userID);
+
+        list = new ArrayList<>();
+        ListView listView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+
+    //Get datasnapshot at your "users" root node
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Course");
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        collectCourses((Map<String,Object>) dataSnapshot.getValue());
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
+        /*
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                    AddCourseActivity.Course course = (AddCourseActivity.Course) dataSnapshot.getValue(AddCourseActivity.Course.class);
+                    String courseString = String.valueOf(course);
+                    adapter.add(courseString);
+
+                    /*String value = dataSnapshot.getValue(String.class);
+                    list.add(value);
+                    adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
 
     }
+
+
 
     @Override
     public void onClick(View v) {
         Button addCourse = (Button) findViewById(R.id.addCourseButton);
         Intent intent = new Intent(this,AddCourseActivity.class);
+
+
+        intent.putExtra("userID", userID);
         startActivityForResult(intent, REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-            if (data.hasExtra("id")) {
-
-            }
-        }
     }
 }
